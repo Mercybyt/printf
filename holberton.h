84dc5bc13cf14c57283116ce176a3dc214be65bb
Mercy Daramola <mercydaramola23@gmail.com>
2021-07-26 09:35:25 -0700

    commit

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+typedef struct Cspecs {
+  char cs;
+  void (*f)();
+} cs_t;
+
+void _putchar_c(char c);
+void _putchar(va_list a);
+void print_str(va_list a);
+int _printf(const char *format, ...);
+
+#endif
