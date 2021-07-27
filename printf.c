4e46f11a10864b474ab7ed38e975a4bb94fe998d
Mercy Daramola <mercydaramola23@gmail.com>
2021-07-27 15:18:46 -0700

    new commit

--- printf.c
+++ printf.c
@@ -1,86 +1,96 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 #include "holberton.h"
 
 /**
- *_printf - prints output according to a format.
- *@format: input string.
- *@c: character c
- *@f: pointer to f
- *struct_Cspec - structure definition
- *_putchar - putchar
- *_putchar_c - putchar c
- *print_int - print integer
- *print_str - print string
- *Return: count, number of characters printed
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
-void _putchar_c(char c)
-{
-	write(1, &c, 1);
-}
 
-void _putchar(va_list a)
-{
-	char c;
-
-	c = va_arg(a, int);
-	write(1, &c, 1);
-}
-
-void print_str(va_list a)
-{
-	char *c;
-
-	c = va_arg(a, char *);
-	while (*c != '\0')
-	{
-		_putchar_c(*c++);
-	}
-}
-
-void print_int(void)
 int _printf(const char *format, ...)
 {
-	va_list a;
-	int i, j, count;
-}
-	cs_t cspec[] = {
-		{'c', _putchar},
-		{'s', print_str},
-		{'d', print_int},
-		{'i', print_int}
-	};
+  int char_count = 0;
+  int int_buff;
+  char *str;
+  char chr;
+  char sint_buff[10];
+  va_list vaList;
 
-	if (format == NULL)
-		return (0);
+  va_start(vaList, format);
 
-	i = j = count = 0;
-	va_start(a, format);
-	while (format[i])
+  while (*format)
+    {
+      if (*format == '%')
 	{
-		if (format[i] == '%')
-		{
-			j = 0;
-			while (j < 4)
-			{
-				if (format[i + 1] == cspec[j].cs &&
-				    format[i + 1] != '%')
-				{
-					cspec[j].f(a);
-					i++;
-				}
-				j++;
-			}
-			i++;
-			_putchar_c(format[i]);
-		}
-		else
-			_putchar_c(format[i]);
-		count++;
-		i++;
-
+	  format++;
+	  switch(*format)
+	    {
+	    case '%':
+	      _putchar('%');
+	      format++;
+	      char_count++;
+	      break;
+	    case 's':
+	      str = va_arg(vaList, char *);
+	      fputs(str, stdout);
+	      format++;
+	      char_count += strlen(str);
+	      break;
+	    case 'c':
+	      chr = va_arg(vaList, int);
+	      _putchar(chr);
+	      format++;
+	      char_count++;
+	      break;
+	    case 'i':
+	    case 'd':
+	      int_buff = va_arg(vaList, int);
+	      itoa(int_buff, sint_buff, 10);
+	      fputs(sint_buff, stdout);
+	      format++;
+	      char_count += strlen(sint_buff);
+	      break;
+	    case 'u':
+	      int_buff = va_arg(vaList, int);
+	      itoa(int_buff, sint_buff, 10);
+	      fputs(sint_buff, stdout);
+	      format++;
+	      char_count += strlen(sint_buff);
+	      break;
+	    case 'o':
+	      int_buff = va_arg(vaList, int);
+	      itoa(int_buff, sint_buff, 8);
+	      fputs(sint_buff, stdout);
+	      format++;
+	      char_count += strlen(sint_buff);
+	      break;
+	    case 'x':
+	    case 'X':
+	      int_buff = va_arg(vaList, int);
+	      itoa(int_buff, sint_buff, 16);
+	      fputs(sint_buff, stdout);
+	      format++;
+	      char_count += strlen(sint_buff);
+	      break;
+	    case '\0':
+	      break;
+	    default:
+	      _putchar(*format);
+	      format++;
+	      char_count += 2;
+	      break;
+	    }
 	}
-	return (count);
+      else
+	{
+	  _putchar(*format);
+	  format++;
+	  char_count++;
+	}
+    }
+  va_end(vaList);
+  return (char_count);
 }
