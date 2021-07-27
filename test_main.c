86065ed598dc035d920f7494cf014afd31271182
Mercy Daramola <mercydaramola23@gmail.com>
2021-07-27 06:40:54 -0700

    test main

---
+++ test_main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *main - description for main
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char *chr;
+
+	chr = "H";
+	_printf("Character: [%c]\n", chr);
+	return (0);
+}
